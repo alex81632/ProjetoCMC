7ea7c0b73285240f2d145e1944c030362c6db700
LucasNCosta2003 <128256390+LucasNCosta2003@users.noreply.github.com>
2023-11-23 14:20:40 -0300

    Update script.js to add start menu logic

--- src/script.js
+++ src/script.js
@@ -1,3 +1,18 @@
 import Experience from './ThreeApp/ThreeApp.js'
 
-const experience = new Experience(document.querySelector('canvas.webgl'))
+document.addEventListener('DOMContentLoaded', function () {
+    // Get references to the start menu and start button
+    const startMenu = document.getElementById('start-menu');
+    // Add click event listener to the start button
+
+    // Add keydown event listener to the document
+    document.addEventListener('keydown', function (event) {
+        // Check if the pressed key is the "Enter" key (key code 13)
+        if (event.key === 'Enter') {
+            // Hide the start menu when the "Enter" key is pressed
+            startMenu.style.display = 'none';
+            const experience = new Experience(document.querySelector('canvas.webgl'))
+        }
+    });
+    // Function to remove the overlay element
+});
